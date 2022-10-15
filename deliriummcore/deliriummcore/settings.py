c0c4e976eaa19294b990383cb3c3157d57964ea5
Alexander Ortiz <a.ortizhennart@gmail.com>
2022-10-15 01:40:15 -0500

    V2 1

--- deliriummcore/deliriummcore/settings.py
+++ deliriummcore/deliriummcore/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z+m(=-g1gtwnfqcv*0ti!t%goup4b9k0g5a#^u(^1k6rway731'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
